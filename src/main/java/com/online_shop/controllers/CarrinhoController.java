36cdbebb7bb1aed6a0921ae4a25378607c7e31ef
Carlos-Tembe <carlostembe35@gmail.com>
2021-08-07 19:50:51 +0200

    Melhorias no design, melhoria das funcionalidades no carrinho/ TODO: Funcionalidade de pagamento e organizacao da navegabilidade da pagina

--- src/main/java/com/online_shop/controllers/CarrinhoController.java
+++ src/main/java/com/online_shop/controllers/CarrinhoController.java
@@ -2,6 +2,7 @@ package com.online_shop.controllers;
 
 import java.util.HashMap;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.online_shop.models.Artigo_detalhes;
 import com.online_shop.models.Item_venda;
@@ -23,21 +25,25 @@ public class CarrinhoController {
 	private ArtigoDetalhesService artigoService;
 
 	@GetMapping("/add/{id}")
-	private String add(@PathVariable Long id, HttpSession session, Model model) {
+	private String add(@PathVariable Long id, HttpSession session, Model model,
+			@RequestParam(value = "cartPage", required = false) String cartPage) {
 		Artigo_detalhes artigo = artigoService.buscarPorId(id);
 
 		if (session.getAttribute("carrinho") == null) {
+
 			HashMap<Long, Item_venda> carrinho = new HashMap<>();
-			carrinho.put(id, new Item_venda(id, artigo.getArtigo().getDescricao(), artigo.getPreco_unitario(), 1));
+			carrinho.put(id, new Item_venda(id, artigo.getArtigo().getDescricao(), artigo.getPreco_unitario(), 1,
+					artigo.getArtigo().getFoto()));
 			session.setAttribute("carrinho", carrinho);
 		} else {
 			HashMap<Long, Item_venda> carrinho = (HashMap<Long, Item_venda>) session.getAttribute("carrinho");
 			if (carrinho.containsKey(id)) {
 				double qty = carrinho.get(id).getQuantidade();
-				carrinho.put(id,
-						new Item_venda(id, artigo.getArtigo().getDescricao(), artigo.getPreco_unitario(), ++qty));
+				carrinho.put(id, new Item_venda(id, artigo.getArtigo().getDescricao(), artigo.getPreco_unitario(),
+						++qty, artigo.getArtigo().getFoto()));
 			} else {
-				carrinho.put(id, new Item_venda(id, artigo.getArtigo().getDescricao(), artigo.getPreco_unitario(), 1));
+				carrinho.put(id, new Item_venda(id, artigo.getArtigo().getDescricao(), artigo.getPreco_unitario(), 1,
+						artigo.getArtigo().getFoto()));
 				session.setAttribute("carrinho", carrinho);
 			}
 		}
@@ -53,7 +59,57 @@ public class CarrinhoController {
 		model.addAttribute("tamanho", tamanho);
 		model.addAttribute("total", total);
 
+		if (cartPage != null) {
+			return "redirect:/carrinho/view";
+		}
 		return "fragments/carrinho_view";
+	}
+
+	@GetMapping("/subtract/{id}")
+	private String subtract(@PathVariable Long id, HttpSession session, Model model,
+			HttpServletRequest httpServletRequest) {
+		Artigo_detalhes artigo = artigoService.buscarPorId(id);
+		HashMap<Long, Item_venda> carrinho = (HashMap<Long, Item_venda>) session.getAttribute("carrinho");
+
+		double qty = carrinho.get(id).getQuantidade();
+
+		if (qty == 1) {
+			carrinho.remove(id);
+			if (carrinho.size() == 0) {
+				session.removeAttribute("carrinho");
+			}
+		} else {
+
+			carrinho.put(id, new Item_venda(id, artigo.getArtigo().getDescricao(), artigo.getPreco_unitario(), --qty,
+					artigo.getArtigo().getFoto()));
+		}
+
+		String refererLink = httpServletRequest.getHeader("referer");
+		return "redirect:" + refererLink;
+
+	}
+
+	@GetMapping("/clear")
+	private String remove(HttpSession session, HttpServletRequest httpServletRequest) {
+		session.removeAttribute("carrinho");
+		String refererLink = httpServletRequest.getHeader("referer");
+		return "redirect:" + refererLink;
+
+	}
+
+	@GetMapping("/remove/{id}")
+	private String remove(@PathVariable Long id, HttpSession session, HttpServletRequest httpServletRequest) {
+
+		HashMap<Long, Item_venda> carrinho = (HashMap<Long, Item_venda>) session.getAttribute("carrinho");
+
+		carrinho.remove(id);
+		if (carrinho.size() == 0) {
+			session.removeAttribute("carrinho");
+		}
+
+		String refererLink = httpServletRequest.getHeader("referer");
+		return "redirect:" + refererLink;
+
 	}
 
 	@RequestMapping("/view")
@@ -62,8 +118,17 @@ public class CarrinhoController {
 			return "redirect:/";
 		}
 		HashMap<Long, Item_venda> carrinho = (HashMap<Long, Item_venda>) session.getAttribute("carrinho");
+		int tamanho = 0;
+		double total = 0;
+		for (Item_venda value : carrinho.values()) {
+			tamanho += value.getQuantidade();
+			total += value.getPreco_unitario() * value.getQuantidade();
+		}
+
+		model.addAttribute("tamanho", tamanho);
+		model.addAttribute("total", total);
 		model.addAttribute("carrinho", carrinho);
 
-		return "carrinho";
+		return "fragments/carrinho";
 	}
 }
--- src/main/java/com/online_shop/models/Item_venda.java
+++ src/main/java/com/online_shop/models/Item_venda.java
@@ -4,10 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "ITEM_VENDA")
@@ -21,13 +23,35 @@ public class Item_venda extends AbstractEntity<Long> {
 	private double quantidade;
 
 	private double item_montante;
+	private String foto;
+	private Artigo_detalhes detalheArtigo;
 
-	public Item_venda(long artigo_id, String artigo_nome, double preco_unitario, double quantidade) {
+	public Item_venda(long artigo_id, String artigo_nome, double preco_unitario, double quantidade, String foto,
+			double item_montante) {
 
 		super();
 		this.artigo_id = artigo_id;
 		this.artigo_nome = artigo_nome;
 		this.preco_unitario = preco_unitario;
+		this.quantidade = quantidade;
+		this.item_montante = item_montante;
+		this.foto = foto;
+	}
+
+	public Item_venda(long artigo_id, String artigo_nome, double preco_unitario, double quantidade, String foto) {
+
+		super();
+		this.artigo_id = artigo_id;
+		this.artigo_nome = artigo_nome;
+		this.preco_unitario = preco_unitario;
+		this.quantidade = quantidade;
+		this.foto = foto;
+	}
+
+	public Item_venda(Artigo_detalhes detalhes, double quantidade) {
+
+		super();
+		this.detalheArtigo = detalhes;
 		this.quantidade = quantidade;
 	}
 
